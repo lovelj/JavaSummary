bf96338ed7cc556e1dbd877f94f58b7c10bb4f19
lianjie <976825262@qq.com>
2020-05-25 17:48:52 +0800

    盛最多水的容器

--- leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/arraydemo.java
+++ leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/arraydemo.java
@@ -1,4 +1,33 @@
 package org.lj.leetcodedemo.arraydemo;
 
 public class arraydemo {
+    /**
+     * 11. 盛最多水的容器
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int leftindex =0;
+        int rightindex = height.length-1;
+
+        int leftvalue =  height[leftindex];
+        int rightvalue = height[rightindex];
+        int maxarea = (rightindex-leftindex)*(Math.min(rightvalue,leftvalue));
+        while(rightindex>leftindex){
+
+            if(height[leftindex]<height[rightindex]){
+                leftindex++;
+            }else{
+                rightindex--;
+            }
+            leftvalue =  height[leftindex];
+            rightvalue = height[rightindex];
+            int curarea=(rightindex-leftindex)*(Math.min(rightvalue,leftvalue));
+            if(curarea>maxarea){
+                maxarea=curarea;
+            }
+
+        }
+        return maxarea;
+    }
 }
---
+++ leetcode/src/test/java/org/lj/leetcodedemo/arraydemo/arraydemoTest.java
@@ -0,0 +1,16 @@
+package org.lj.leetcodedemo.arraydemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class arraydemoTest {
+
+    @Test
+    public void maxArea() {
+        int input[] = {1,8,6,2,5,4,8,3,7};
+        arraydemo demo = new arraydemo();
+        demo.maxArea(input);
+
+    }
+}
