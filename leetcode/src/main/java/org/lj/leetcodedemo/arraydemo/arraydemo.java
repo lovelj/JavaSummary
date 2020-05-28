84fe9dbd01658241e7a8320a0eed5afcd5af2d5a
lianjie <976825262@qq.com>
2020-05-28 13:56:30 +0800

    搜索旋转排序数组

--- leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/arraydemo.java
+++ leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/arraydemo.java
@@ -1,5 +1,6 @@
 package org.lj.leetcodedemo.arraydemo;
 
+import javax.xml.bind.annotation.XmlInlineBinaryData;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -164,4 +165,65 @@ public class arraydemo {
         return i+1;
 
     }
+
+    /**
+     *27. 移除元素
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement(int[] nums, int val) {
+        if(nums.length<1) return 0;
+        int i=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=val){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
+    /**
+     * 31. 下一个排列
+     * @param nums
+     */
+    public void nextPermutation(int[] nums) {
+
+    }
+
+    /**
+     * 33. 搜索旋转排序数组
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        if(nums.length<1) return -1;
+        int l=0;
+        int r=nums.length-1;
+        int mid=0;
+        while(l<=r){
+            mid = l+(r-l)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+
+            if(nums[l]<=nums[mid]){
+                if(target>=nums[l] && target<nums[mid]){
+                    r=mid-1;
+                }else {
+                    l=mid+1;
+                }
+            }else{
+                if(target>nums[mid]&&target<=nums[r]){
+                    l=mid+1;
+                }else {
+                    r=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+
 }
