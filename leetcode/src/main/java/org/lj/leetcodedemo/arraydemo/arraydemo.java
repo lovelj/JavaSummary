b4a8591a77b1f7bf52d5adcaf52a117501ae7da4
lianjie <976825262@qq.com>
2020-05-29 10:15:48 +0800

    35. 搜索插入位置

--- leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/arraydemo.java
+++ leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/arraydemo.java
@@ -260,4 +260,29 @@ public class arraydemo {
         return new int[]{-1,-1};
     }
 
+    /**
+     *35. 搜索插入位置
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchInsert(int[] nums, int target) {
+        if(nums.length<1) return 0;
+        int start = 0;
+        int end = nums.length-1;
+        int mid =0;
+        while(start<=end){
+            mid = start +(end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                end=mid-1;
+            }else if(nums[mid]<target){
+                start=mid+1;
+            }
+
+        }
+        return start;
+    }
+
 }
