16255012ccf3b4a0703dd8ca5a0ef19a9651ebe0
lianjie <976825262@qq.com>
2020-06-01 14:54:16 +0800

    39组合总和

--- leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/arraydemo.java
+++ leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/arraydemo.java
@@ -1,8 +1,6 @@
 package org.lj.leetcodedemo.arraydemo;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class arraydemo {
     /**
@@ -284,5 +282,110 @@ public class arraydemo {
         }
         return start;
     }
+
+
+    /**
+     * 39. 组合总和
+     * @param candidates
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        int len = candidates.length;
+
+        // 排序是为了提前终止搜索
+        Arrays.sort(candidates);
+
+        dfs2(candidates, len, target, 0, new ArrayDeque<>(), res);
+        return res;
+    }
+
+
+    private void dfs2(int[] candidates,int len,int dis,int begin,Deque<Integer> path,List<List<Integer>> res){
+        if(dis==0){
+            res.add(new ArrayList<>(path));
+        }
+        for(int i=begin;i<len;i++){
+            // 在数组有序的前提下，剪枝
+            if (dis - candidates[i] < 0) {
+                break;
+            }
+
+            path.add(candidates[i]);
+            dfs2(candidates, len, dis-candidates[i],i,path, res);
+            path.removeLast();
+        }
+
+    }
+
+    /**
+     * @param candidates 数组输入
+     * @param len        输入数组的长度，冗余变量
+     * @param residue    剩余数值
+     * @param begin      本轮搜索的起点下标
+     * @param path       从根结点到任意结点的路径
+     * @param res        结果集变量
+     */
+    private void dfs(int[] candidates,
+                     int len,
+                     int residue,
+                     int begin,
+                     Deque<Integer> path,
+                     List<List<Integer>> res) {
+        if (residue == 0) {
+            // 由于 path 全局只使用一份，到叶子结点的时候需要做一个拷贝
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = begin; i < len; i++) {
+
+            // 在数组有序的前提下，剪枝
+            if (residue - candidates[i] < 0) {
+                break;
+            }
+
+            path.addLast(candidates[i]);
+            dfs(candidates, len, residue - candidates[i], i, path, res);
+            path.removeLast();
+        }
+    }
+
+    /**
+     * 40. 组合总和 II
+     * @param candidates
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        int len = candidates.length;
+
+        // 排序是为了提前终止搜索
+        Arrays.sort(candidates);
+        dfs22(candidates,len,0,target, new ArrayDeque<>(),res);
+        return res;
+    }
+
+    private void dfs22(int[] candidates,int len,int start,int diff,Deque<Integer> paths,List<List<Integer>> res){
+        if(diff==0){
+            res.add(new ArrayList<>(paths));
+            return;
+        }
+
+        for(int i=start;i<len;i++){
+            if(candidates[i]>diff){
+                break;
+            }
+
+            if(i>start && candidates[i]==candidates[i-1]){
+                continue;
+            }
+            paths.addLast(candidates[i]);
+            dfs22(candidates, len, i+1,diff - candidates[i], paths, res);
+            paths.removeLast();
+        }
+    }
+
 
 }
