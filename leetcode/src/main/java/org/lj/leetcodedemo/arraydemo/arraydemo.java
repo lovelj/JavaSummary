592f96560147f10f6c986962660bf4fa1399b07f
lianjie <976825262@qq.com>
2020-06-03 16:14:45 +0800

    53. 最大子序和

--- leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/arraydemo.java
+++ leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/arraydemo.java
@@ -414,6 +414,44 @@ public class arraydemo {
         }
     }
 
+    /**
+     * 53. 最大子序和
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        int len = nums.length;
+        if(len<1){
+            return 0;
+        }
+        int[] dp=new int[len];
+        dp[0]=nums[0];
+        for(int i=1;i<len;i++){
+            if(dp[i-1]>=0){
+                dp[i]=dp[i-1]+nums[i];
+            }else{
+                dp[i]=nums[i];
+            }
+        }
+        int res =dp[0];
+        for(int i=1;i<len;i++){
+            res=Math.max(res,dp[i]);
+        }
+        return  res;
+
+//        int ans = nums[0];
+//        int sum = 0;
+//        for(int num: nums) {
+//            if(sum > 0) {
+//                sum += num;
+//            } else {
+//                sum = num;
+//            }
+//            ans = Math.max(ans, sum);
+//        }
+//        return ans;
+    }
+
 
 
 }
