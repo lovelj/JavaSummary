7b70cdfbd66d3397a7f9d7bfd0055bc10976fc3b
lianjie <976825262@qq.com>
2020-06-04 20:44:30 +0800

    64. 最小路径和

--- leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/arraydemo.java
+++ leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/arraydemo.java
@@ -545,4 +545,62 @@ public class arraydemo {
         return dp[m - 1][n - 1];
     }
 
+    /**
+     *63. 不同路径 II
+     * @param obstacleGrid
+     * @return
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int row=obstacleGrid.length;
+        int col=obstacleGrid[0].length;
+
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+
+        obstacleGrid[0][0] = 1;
+        for (int i = 1; i < row; i++) {
+            obstacleGrid[i][0] = (obstacleGrid[i][0] == 0 && obstacleGrid[i - 1][0] == 1) ? 1 : 0;
+        }
+
+        for (int i = 1; i < col; i++) {
+            obstacleGrid[0][i] = (obstacleGrid[0][i] == 0 && obstacleGrid[0][i - 1] == 1) ? 1 : 0;
+        }
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+                } else {
+                    obstacleGrid[i][j] = 0;
+                }
+            }
+        }
+
+        return obstacleGrid[row - 1][col - 1];
+    }
+
+    /**
+     * 64. 最小路径和
+     * @param grid
+     * @return
+     */
+    public int minPathSum(int[][] grid) {
+        int row=grid.length;
+        int col=grid[0].length;
+
+        int[][] dp = new int[row][col];
+        dp[0][0]= grid[0][0];
+
+        for (int i = 1; i < col; i++) dp[0][i] = grid[0][i]+dp[0][i-1];
+        for (int i = 1; i < row; i++) dp[i][0] = grid[i][0]+dp[i-1][0];
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1])+grid[i][j];
+            }
+        }
+
+        return dp[row-1][col-1];
+    }
+
 }
