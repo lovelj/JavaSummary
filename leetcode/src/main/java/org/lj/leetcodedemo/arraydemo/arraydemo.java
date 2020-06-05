9803d06833ca835e55423b4c4930bad4ba1e4168
lovelj <976825262@qq.com>
2020-06-05 22:46:52 +0800

    73. 矩阵置零

--- leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/arraydemo.java
+++ leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/arraydemo.java
@@ -609,17 +609,66 @@ public class arraydemo {
      * @return
      */
     public int[] plusOne(int[] digits) {
-        int n = digits.length;
-        for(int i = n -1;i>=0;i--){
-            if(digits[i] == 9){
-                digits[i] = 0;
+        int len = digits.length;
+        for(int i=len-1;i>=0;i--){
+            if(digits[i]==9){
+                digits[i]=0;
             }else{
-                digits[i]++;
+                digits[i]=digits[i]+1;
                 return digits;
             }
         }
-        int[] a = new int [n+1];
-        a[0]=1;
-        return a;
+        int digsn[]=new int[len+1];
+        digsn[0]=1;
+        return digsn;
+
+//        int n = digits.length;
+//        for(int i = n -1;i>=0;i--){
+//            if(digits[i] == 9){
+//                digits[i] = 0;
+//            }else{
+//                digits[i]++;
+//                return digits;
+//            }
+//        }
+//        int[] a = new int [n+1];
+//        a[0]=1;
+//        return a;
     }
+
+    public void setZeroes(int[][] matrix) {
+        int MODIFIED = -1000000;
+        int R = matrix.length;
+        int C = matrix[0].length;
+
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                if (matrix[r][c] == 0) {
+                    // We modify the corresponding rows and column elements in place.
+                    // Note, we only change the non zeroes to MODIFIED
+                    for (int k = 0; k < C; k++) {
+                        if (matrix[r][k] != 0) {
+                            matrix[r][k] = MODIFIED;
+                        }
+                    }
+                    for (int k = 0; k < R; k++) {
+                        if (matrix[k][c] != 0) {
+                            matrix[k][c] = MODIFIED;
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                // Make a second pass and change all MODIFIED elements to 0 """
+                if (matrix[r][c] == MODIFIED) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+    }
+
+
 }
