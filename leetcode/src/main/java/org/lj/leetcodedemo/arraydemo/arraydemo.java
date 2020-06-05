8132e2f3cc1c3f79e638fe010113a6171a9d1b5f
lovelj <976825262@qq.com>
2020-06-05 22:27:43 +0800

    66. 加一

--- leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/arraydemo.java
+++ leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/arraydemo.java
@@ -603,4 +603,23 @@ public class arraydemo {
         return dp[row-1][col-1];
     }
 
+    /**
+     * 66. 加一
+     * @param digits
+     * @return
+     */
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for(int i = n -1;i>=0;i--){
+            if(digits[i] == 9){
+                digits[i] = 0;
+            }else{
+                digits[i]++;
+                return digits;
+            }
+        }
+        int[] a = new int [n+1];
+        a[0]=1;
+        return a;
+    }
 }
