1b0eb51115e72e0a2804996bdc34956ae3c8b5b5
lianjie <976825262@qq.com>
2020-06-08 20:23:10 +0800

    删除重复节点

--- leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/linkedliskdemo.java
+++ leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/linkedliskdemo.java
@@ -142,4 +142,39 @@ public class linkedliskdemo {
         return secondNode;
     }
 
+    /**
+     * 61. 旋转链表
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode rotateRight(ListNode head, int k) {
+
+    }
+
+    /**
+     *83. 删除排序链表中的重复元素
+     * @param head
+     * @return
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.next.val == current.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
+
+    /**
+     * 82. 删除排序链表中的重复元素 II
+     * @param head
+     * @return
+     */
+    public ListNode deleteDuplicates2(ListNode head) {
+
+    }
 }
