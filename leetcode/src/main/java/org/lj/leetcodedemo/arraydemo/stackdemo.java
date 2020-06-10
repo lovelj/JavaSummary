70cb80403aa790048298e9d0e922887e2da489b7
lianjie <976825262@qq.com>
2020-06-10 19:25:49 +0800

    stack

--- leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/stackdemo.java
+++ leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/stackdemo.java
@@ -41,6 +41,55 @@ public class stackdemo {
      * @return
      */
     public String simplifyPath(String path) {
+        Stack<String> stack =new Stack<>();
+        String[] str = path.split("/");
+
+        for(String s:str){
+            if(s.equals("..")){
+                if ( !stack.isEmpty() ) {
+                    stack.pop();
+                }
+            }
+            else if ( !s.equals("") && !s.equals(".") ) {
+                stack.push(s);
+            }
+        }
+
+        if ( stack.isEmpty() ) {
+            return "/";
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < stack.size(); i++) {
+            ans.append( "/" + stack.get(i) );
+        }
+        return ans.toString();
 
     }
+
+    /**
+     * 496. 下一个更大元素 I //todo:
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        Stack < Integer > stack = new Stack < > ();
+        HashMap < Integer, Integer > map = new HashMap < > ();
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums2.length; i++) {
+            while (!stack.empty() && nums2[i] > stack.peek())
+                map.put(stack.pop(), nums2[i]);
+            stack.push(nums2[i]);
+        }
+        while (!stack.empty())
+            map.put(stack.pop(), -1);
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.get(nums1[i]);
+        }
+        return res;
+    }
+
+
 }
