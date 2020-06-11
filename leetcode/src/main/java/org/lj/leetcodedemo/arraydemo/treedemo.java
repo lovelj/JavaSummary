2c7b7a36638fed843d5377a91be2d12545a5b609
lianjie <976825262@qq.com>
2020-06-11 20:14:55 +0800

    104. 二叉树的最大深度

--- leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/treedemo.java
+++ leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/treedemo.java
@@ -78,6 +78,65 @@ public class treedemo {
         return all_trees;
     }
 
+    /**
+     *
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root==null) {
+            return new ArrayList<List<Integer>>();
+        }
+
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        //将根节点放入队列中，然后不断遍历队列
+        queue.add(root);
+        while(queue.size()>0) {
+            //获取当前队列的长度，这个长度相当于 当前这一层的节点个数
+            int size = queue.size();
+            ArrayList<Integer> tmp = new ArrayList<Integer>();
+            //将队列中的元素都拿出来(也就是获取这一层的节点)，放到临时list中
+            //如果节点的左/右子树不为空，也放入队列中
+            for(int i=0;i<size;++i) {
+                TreeNode t = queue.remove();
+                tmp.add(t.val);
+                if(t.left!=null) {
+                    queue.add(t.left);
+                }
+                if(t.right!=null) {
+                    queue.add(t.right);
+                }
+            }
+            //将临时list加入最终返回结果中
+            res.add(tmp);
+        }
+        return res;
+    }
+
+    /**
+     *104. 二叉树的最大深度
+     * @param root
+     * @return
+     */
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        TreeNode curnode = root;
+        int h=0;
+        if (curnode.left!=null|| curnode.right!=null){
+
+            int lh = maxDepth(curnode.left);
+            int rh = maxDepth(curnode.right);
+            return h+Math.max(lh,rh);
+        }else{
+            return 1;
+        }
+    }
+
+
+
 
 
 }
