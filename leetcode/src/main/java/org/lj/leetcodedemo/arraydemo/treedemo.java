df06103313520912c5b8dbcc753247542fbaf1e8
lianjie <976825262@qq.com>
2020-06-13 18:01:58 +0800

    二叉树的最小深度

--- leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/treedemo.java
+++ leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/treedemo.java
@@ -183,4 +183,28 @@ public class treedemo {
 
     }
 
+    /**
+     *111. 二叉树的最小深度
+     * @param root
+     * @return
+     */
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if ((root.left == null) && (root.right == null)) {
+            return 1;
+        }
+
+        int min_depth = Integer.MAX_VALUE;
+        if (root.left != null) {
+            min_depth = Math.min(minDepth(root.left), min_depth);
+        }
+        if (root.right != null) {
+            min_depth = Math.min(minDepth(root.right), min_depth);
+        }
+
+        return min_depth + 1;
+    }
 }
