d2a5067dc46724590c4406f8f4e1dc8ab4a46269
lianjie <976825262@qq.com>
2020-06-21 21:12:52 +0800

    bst

--- leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/treedemo.java
+++ leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/treedemo.java
@@ -207,4 +207,22 @@ public class treedemo {
 
         return min_depth + 1;
     }
+
+    /**
+     * 二叉搜索树数目
+     * @param n
+     * @return
+     */
+    public int numTrees(int n) {
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
 }
