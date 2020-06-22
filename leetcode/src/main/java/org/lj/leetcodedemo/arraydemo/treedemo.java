4d286a52b4ea274a519dee6d63d2dc4ab6e27d14
lianjie <976825262@qq.com>
2020-06-22 15:03:23 +0800

    平衡二叉树

--- leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/treedemo.java
+++ leetcode/src/main/java/org/lj/leetcodedemo/arraydemo/treedemo.java
@@ -320,5 +320,29 @@ public class treedemo {
         find(root.right, level + 1, result);
     }
 
+    /**
+     * 110 平衡二叉树
+     * @param root
+     * @return
+     */
+    public boolean isBalanced(TreeNode root) {
+// An empty tree satisfies the definition of a balanced tree
+        if (root == null) {
+            return true;
+        }
+
+        // Check if subtrees have height within 1. If they do, check if the
+        // subtrees are balanced
+        return Math.abs(height(root.left) - height(root.right)) < 2
+                && isBalanced(root.left)
+                && isBalanced(root.right);
+    }
+    private int height(TreeNode root) {
+        // An empty tree has height -1
+        if (root == null) {
+            return -1;
+        }
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
 
 }
